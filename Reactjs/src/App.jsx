ecf70a2b9d2a6cccac8594091dc1e755dc6f412f
jagadeesh056 <jagadeeshsiddani01@gmail.com>
2024-03-10 18:35:48 +0530

    initial commit

---
+++ Reactjs/src/App.jsx
@@ -0,0 +1,27 @@
+import  { useState } from 'react';
+
+function CounterApp() {
+  // State variable to hold the count
+  const [count, setCount] = useState(0);
+
+  // Function to increment the count
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  // Function to decrement the count
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default CounterApp;
